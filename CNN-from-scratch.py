0c9dcf1dc008803a96d6b824bfe3b4f76fbee13b
Millad Dagdoni <milladd@gmail.com>
2025-08-07 21:05:35 +0200

    Update CNN-from-scratch.py
    simplified the program run code. Removed main method because it is not needed.

--- CNN-from-scratch.py
+++ CNN-from-scratch.py
@@ -3,9 +3,6 @@
 
 import numpy as np
 
-# INIT
-alpha = 0.04
-
 kernel_vertical = np.array([
     (0, 0, 0, 1, 0, 0, 0),
     (0, 0, 0, 1, 0, 0, 0),
@@ -51,7 +48,6 @@ img_data_horiz_line = np.array([
 relu = lambda x: (x >= 0) * x  # returns x if x > 0, return 0 otherwise
 relu2deriv = lambda x: x > 0  # returns 1 for input > 0, return 0 otherwise
 
-
 def conv(img_data, kernel):
     layer_0_data_input_list = list()
     for i in range(3):
@@ -60,28 +56,28 @@ def conv(img_data, kernel):
             layer_1 = np.sum(layer_0 * kernel)
             layer_0_data_input_list.append(layer_1)
     return np.array(layer_0_data_input_list)
-y = 1
-if __name__ == "__main__":
-    for training_run in range(115):
-        # Forward propegation
-        layer_1 = conv(img_data_vertical_line, kernel_vertical)
-        layer_2 = relu(np.dot(layer_1, weights))
-        # Cost
-        error = (layer_2 - y) ** 2
-        # Backpropegation
 
-        layer_2_delta = ((layer_2 - y) * relu2deriv(layer_1))
-        weights -= alpha * (layer_2_delta)
-        if (training_run % 10 == 9):
-            print("[vertikal bilde error]: " + str(error))
+alpha = 0.04
+y = 1
+
+for training_run in range(115):
+    # Forward propegation
+    layer_1 = conv(img_data_vertical_line, kernel_vertical)
+    layer_2 = relu(np.dot(layer_1, weights))
+
+    # Cost
+    error = (layer_2 - y) ** 2
+    
+    # Backpropegation
+    layer_2_delta = ((layer_2 - y) * relu2deriv(layer_1))
+    weights -= alpha * (layer_2_delta)
+    if (training_run % 10 == 9):
+        print("[vertical image error]: ", error)
 
 # Test HORIZONTAL image should have high error
 layer_1 = conv(img_data_horiz_line, kernel_horiz)
 layer_2 = relu(np.dot(layer_1, weights))
 error = (layer_2 - y) ** 2
-print("[HORIZONTAL image should have high rror]: " + str(error))
 
-
-print("program end")
-# Program SLUTT
-# Millad Dagdoni
+print("[HORIZONTAL image should have high error]: ", error)
+print("Program end")
